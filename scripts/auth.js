d537d45154db4b5f3f111418f7d2b6e42c908055
Priyam Sheth <87331766+pjsheth354@users.noreply.github.com>
2023-05-07 02:13:14 +0530

    Add files via upload

---
+++ scripts/auth.js
@@ -0,0 +1,43 @@
+const loginForm = document.querySelector('#loginForm')
+loginForm.addEventListener('submit',(e) => {
+  
+  e.preventDefault();
+
+  const email = loginForm['username'].value;
+  const password = loginForm['password'].value;
+  auth.signInWithEmailAndPassword(email,password).then((usercredential)=>{
+    if (usercredential.uid == "WprNPOxnD8OX4EEFqvm3Rx1VYnv1"){
+        alert("Login Successfull");
+        location.href = "adminpage.html"
+    }
+    else if (usercredential.uid == "oPeap0ViqtWGdxHO90r1BnPQXXq1"){
+        alert("Login Successfull");
+        location.href = "./Plant Page/tata-mi-my-plantpage.html"
+    }
+    else if (usercredential.uid == "8j0erjtfV4NQSOMrYZXET2OUAiU2"){
+        alert("Login Successfull");
+        location.href = "./Plant Page/tprel-dholera-plantpage.html"
+    }
+    else if (usercredential.uid == "RPnUhffMMScm3RJwfdcOABHx7Mj1"){
+        alert("Login Successfull");
+        location.href = "./Plant Page/tprel-raghanesda-plantpage.html"
+    }
+    else if (usercredential.uid == "Cwml0bSm6JeB4w9UlT2F4INjr373"){
+        alert("Login Successfull");
+        location.href = "./Plant Page/ujjawala-plantpage.html"
+    }
+    else if (usercredential.uid == "Owm9KoRfzjf7mzJiVfoYCztUdm52"){
+        alert("Login Successfull");
+        location.href = "./Plant Page/visual-percept-plantpage.html"
+    }
+    else{
+        alert("Enter a valid username and password");
+    }
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode);
+    alert(errorMessage);
+    // ..
+  });
+})
---
+++ scripts/logout.js
@@ -0,0 +1,23 @@
+const logoutForm = document.querySelector(".logoutForm");
+logoutForm.addEventListener('click',(e) => {
+    e.preventDefault();
+    auth.signOut().then(()=>{
+        console.log("User Signed Out Successfully");
+        location.href = "index.html"
+    })
+})
+
+var profile_email
+auth.onAuthStateChanged((user) => {
+    if(user){
+        console.log("User is logged in");
+        profile_email = user.email;
+    }
+    else{
+        location.href = "index.html"
+    }
+})
+
+function profilebtn(){
+    alert(profile_email)
+}
